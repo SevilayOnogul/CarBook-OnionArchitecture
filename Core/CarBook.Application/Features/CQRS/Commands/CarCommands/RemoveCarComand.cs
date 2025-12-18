aa795f4d42caa1377aeadad3d113d168b69b7019
SevilayOnogul <sevilayonogul34@hotmail.com>
2025-12-19 00:29:18 +0300

    Add project files.

---
+++ Core/CarBook.Application/Features/CQRS/Commands/CarCommands/RemoveCarComand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Commands.CarCommands
+{
+    public class RemoveCarComand
+    {
+        public RemoveCarComand(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
